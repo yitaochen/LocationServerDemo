3e929c526ba05bb96143fe5f5ff4a8c7a2b29fb3
vampirefan <vampirefanmail@gmail.com>
2012-11-16 23:50:46 +0800

    Updated at Fri Nov 16 23:50:46 CST 2012

--- mongo.js
+++ mongo.js
@@ -7,16 +7,27 @@ var Server = mongo.Server;
 var Db = mongo.Db;
 
 function start() {
-    var server = new Server('localhost', 27017, {
-        auto_reconnect: true
-    });
-    var db = new Db('exampleDb', server);
+	var server = new Server('localhost', 27017, {
+		auto_reconnect: true
+	});
+	var db = new Db('exampleDb', server);
 
-    db.open(function(err, db) {
-        if(!err) {
-            console.log("We are connected");
-        }
-    });
+	db.open(function(err, db) {
+		if(!err) {
+			console.log("We are connected");
+			try {
+				// file = fs.readFileSync("./db/finger.json", "utf8");
+				// var fingerdb = JSON.parse(file);
+				// db.collection('test', function(err, collection) {
+				// collection.save(fingerdb);
+				// });
+			} catch(e) {
+				console.log(e.name + ": " + e.message);
+			}
+		}
+	});
 }
+
+
 
 exports.start = start;
