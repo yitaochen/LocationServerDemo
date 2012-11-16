bbcac09862b8ff9233db7dd7c763d8f1a660eefc
vampirefan <vampirefanmail@gmail.com>
2012-11-16 15:11:56 +0800

    ok

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var hostAddress = "127.0.0.1";
 var os = require('os');
 var interfaces = os.networkInterfaces();
 var addresses = [];
-for(k in interfaces) {
-    for(k2 in interfaces[k]) {
+for(var k in interfaces) {
+    for(var k2 in interfaces[k]) {
         var address = interfaces[k][k2];
         if(address.family == 'IPv4' && !address.internal) {
             addresses.push(address.address)
